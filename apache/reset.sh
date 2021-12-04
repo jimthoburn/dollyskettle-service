cb0fc26cec906735f000333e71600fbaf7b532ee
Jim Thoburn <jim@jimthoburn.com>
2021-12-04 12:01:07 -0800

    Add script for reseting Wordpress and MySQL

---
+++ apache/reset.sh
@@ -0,0 +1,87 @@
+#!/bin/sh
+
+echo "- - - - - - - - - - - - - - - - - - - - - - -"
+echo "Starting reset"
+echo "- - - - - - - - - - - - - - - - - - - - - - -"
+
+echo "- - - - - - - - - - - - - - - - - - - - - - -"
+echo "cd to /var/www"
+echo "- - - - - - - - - - - - - - - - - - - - - - -"
+
+cd /var/www
+
+echo "- - - - - - - - - - - - - - - - - - - - - - -"
+echo "Configure Git"
+echo "- - - - - - - - - - - - - - - - - - - - - - -"
+
+git config --global user.email $GITHUB_USER_EMAIL
+git config --global user.name "$GITHUB_USER_NAME"
+git lfs install
+
+rm -rf /var/www/.ssh
+mkdir /var/www/.ssh
+cp /etc/secrets/id_ed25519 /var/www/.ssh/id_ed25519
+cp /etc/secrets/id_ed25519.pub /var/www/.ssh/id_ed25519.pub
+cp /etc/secrets/known_hosts /var/www/.ssh/known_hosts
+
+# https://unix.stackexchange.com/questions/31947/how-to-add-a-newline-to-the-end-of-a-file
+sed -i -e '$a\' /var/www/.ssh/id_ed25519
+sed -i -e '$a\' /var/www/.ssh/id_ed25519.pub
+sed -i -e '$a\' /var/www/.ssh/known_hosts
+
+chmod 600 /var/www/.ssh/id_ed25519
+chmod 600 /var/www/.ssh/id_ed25519.pub
+chmod 600 /var/www/.ssh/known_hosts
+
+echo "- - - - - - - - - - - - - - - - - - - - - - -"
+echo "Put site in maintenance mode"
+echo "- - - - - - - - - - - - - - - - - - - - - - -"
+
+echo "<?php \$upgrading = time(); ?>" > /var/www/html/.maintenance
+
+echo "- - - - - - - - - - - - - - - - - - - - - - -"
+echo "cd to /var/www/git-wordpress"
+echo "- - - - - - - - - - - - - - - - - - - - - - -"
+
+cd /var/www/git-wordpress
+
+echo "- - - - - - - - - - - - - - - - - - - - - - -"
+echo "Get latest files, while stashing the maintenance file"
+echo "- - - - - - - - - - - - - - - - - - - - - - -"
+
+eval "$(ssh-agent -s)"
+git reset
+git pull --autostash
+
+echo "- - - - - - - - - - - - - - - - - - - - - - -"
+echo "Delete MySQL database"
+echo "- - - - - - - - - - - - - - - - - - - - - - -"
+
+mysql \
+  -h $WORDPRESS_DB_HOST \
+  -u $WORDPRESS_DB_USER \
+  --password=$WORDPRESS_DB_PASSWORD $WORDPRESS_DB_NAME
+
+drop database wordpress;
+create database wordpress;
+exit;
+
+echo "- - - - - - - - - - - - - - - - - - - - - - -"
+echo "Import MySQL database"
+echo "- - - - - - - - - - - - - - - - - - - - - - -"
+
+mysql \
+  -h $WORDPRESS_DB_HOST \
+  -u $WORDPRESS_DB_USER \
+  --password=$WORDPRESS_DB_PASSWORD $WORDPRESS_DB_NAME \
+  < /var/www/git-wordpress/wordpress-database.sql
+
+echo "- - - - - - - - - - - - - - - - - - - - - - -"
+echo "Put site back in non-maintenance mode"
+echo "- - - - - - - - - - - - - - - - - - - - - - -"
+
+rm /var/www/html/.maintenance
+
+echo "- - - - - - - - - - - - - - - - - - - - - - -"
+echo "Finished backing up"
+echo "- - - - - - - - - - - - - - - - - - - - - - -"
