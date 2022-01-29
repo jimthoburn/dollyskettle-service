4963fe75ec00b0c3109a3e02c547339624aa25cd
Jim Thoburn <jim@jimthoburn.com>
2022-01-29 14:24:22 -0800

    Avoid locked git error when starting up

--- apache/docker-entrypoint.sh
+++ apache/docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Avoid “Another git process seems to be running in this repository” error
+rm -f /var/www/git-wordpress/.git/index.lock
+
 # Run remote backup, without waiting for it to finish
 # https://unix.stackexchange.com/questions/86247/what-does-ampersand-mean-at-the-end-of-a-shell-script-line#answer-86253
 sh /usr/local/bin/wordpress-backup-remote.sh &
