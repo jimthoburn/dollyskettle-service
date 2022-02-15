65bc1216c94bf25c0b6e8097b05f2b7fb1d1ae68
Jim Thoburn <jim@jimthoburn.com>
2022-02-14 18:42:46 -0800

    Check environment before running automation

--- backup/start.sh
+++ backup/start.sh
@@ -1,30 +1,34 @@
 #!/bin/sh
 
-echo "- - - - - - - - - - - - - - - - - - - - - - -"
-echo "Stop backup status"
-echo "- - - - - - - - - - - - - - - - - - - - - - -"
+if [ "$WORDPRESS_ENVIRONMENT" = "remote-backup" ]; then
 
-curl --request POST \
-     --url "https://api.render.com/v1/services/$BACKUP_STATUS_SERVICE_ID/suspend" \
-     --header 'Accept: application/json' \
-     --header "Authorization: Bearer $BACKUP_STATUS_API_TOKEN"
+  echo "- - - - - - - - - - - - - - - - - - - - - - -"
+  echo "Stop backup status"
+  echo "- - - - - - - - - - - - - - - - - - - - - - -"
 
-echo "- - - - - - - - - - - - - - - - - - - - - - -"
-echo "Start remote backup"
-echo "- - - - - - - - - - - - - - - - - - - - - - -"
+  curl --request POST \
+       --url "https://api.render.com/v1/services/$BACKUP_STATUS_SERVICE_ID/suspend" \
+       --header 'Accept: application/json' \
+       --header "Authorization: Bearer $BACKUP_STATUS_API_TOKEN"
 
-curl -X POST -d '{}' "$REMOTE_BACKUP_DEPLOY_HOOK"
+  echo "- - - - - - - - - - - - - - - - - - - - - - -"
+  echo "Start remote backup"
+  echo "- - - - - - - - - - - - - - - - - - - - - - -"
 
-curl --request POST \
-     --url "https://api.render.com/v1/services/$REMOTE_BACKUP_MYSQL_SERVICE_ID/resume" \
-     --header 'Accept: application/json' \
-     --header "Authorization: Bearer $REMOTE_BACKUP_API_TOKEN"
+  curl -X POST -d '{}' "$REMOTE_BACKUP_DEPLOY_HOOK"
 
-curl --request POST \
-     --url "https://api.render.com/v1/services/$REMOTE_BACKUP_WORDPRESS_SERVICE_ID/resume" \
-     --header 'Accept: application/json' \
-     --header "Authorization: Bearer $REMOTE_BACKUP_API_TOKEN"
+  curl --request POST \
+       --url "https://api.render.com/v1/services/$REMOTE_BACKUP_MYSQL_SERVICE_ID/resume" \
+       --header 'Accept: application/json' \
+       --header "Authorization: Bearer $REMOTE_BACKUP_API_TOKEN"
 
-echo "- - - - - - - - - - - - - - - - - - - - - - -"
-echo "Finished"
-echo "- - - - - - - - - - - - - - - - - - - - - - -"
+  curl --request POST \
+       --url "https://api.render.com/v1/services/$REMOTE_BACKUP_WORDPRESS_SERVICE_ID/resume" \
+       --header 'Accept: application/json' \
+       --header "Authorization: Bearer $REMOTE_BACKUP_API_TOKEN"
+
+  echo "- - - - - - - - - - - - - - - - - - - - - - -"
+  echo "Finished"
+  echo "- - - - - - - - - - - - - - - - - - - - - - -"
+
+fi
