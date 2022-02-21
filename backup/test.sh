b285b8780a0695529cfef4001c226b888b2ae347
Jim Thoburn <jim@jimthoburn.com>
2022-02-20 17:31:50 -0800

    Switch off automatic scripts temporarily

--- backup/test.sh
+++ backup/test.sh
@@ -45,19 +45,19 @@ if [ "$WORDPRESS_ENVIRONMENT" = "remote-backup" ]; then
 
     echo '<html><body style="background-color: green; color: white;"><h1>Backup succeeded</h1></body></html>' >> backup-test-results/index.html
 
-    echo "- - - - - - - - - - - - - - - - - - - - - - -"
-    echo "Stopping remote backup"
-    echo "- - - - - - - - - - - - - - - - - - - - - - -"
-
-    curl --request POST \
-         --url "https://api.render.com/v1/services/$REMOTE_BACKUP_WORDPRESS_SERVICE_ID/suspend" \
-         --header 'Accept: application/json' \
-         --header "Authorization: Bearer $REMOTE_BACKUP_API_TOKEN"
-    
-    curl --request POST \
-         --url "https://api.render.com/v1/services/$REMOTE_BACKUP_MYSQL_SERVICE_ID/suspend" \
-         --header 'Accept: application/json' \
-         --header "Authorization: Bearer $REMOTE_BACKUP_API_TOKEN"
+    # echo "- - - - - - - - - - - - - - - - - - - - - - -"
+    # echo "Stopping remote backup"
+    # echo "- - - - - - - - - - - - - - - - - - - - - - -"
+    # 
+    # curl --request POST \
+    #      --url "https://api.render.com/v1/services/$REMOTE_BACKUP_WORDPRESS_SERVICE_ID/suspend" \
+    #      --header 'Accept: application/json' \
+    #      --header "Authorization: Bearer $REMOTE_BACKUP_API_TOKEN"
+    # 
+    # curl --request POST \
+    #      --url "https://api.render.com/v1/services/$REMOTE_BACKUP_MYSQL_SERVICE_ID/suspend" \
+    #      --header 'Accept: application/json' \
+    #      --header "Authorization: Bearer $REMOTE_BACKUP_API_TOKEN"
   fi
 
   # echo "- - - - - - - - - - - - - - - - - - - - - - -"
