f5e56ca849d1372aa8aa13f89676ee15fa72cacc
Jim Thoburn <jim@jimthoburn.com>
2022-02-21 21:04:23 -0800

    Skip publishing after backing up

--- backup/publish.sh
+++ backup/publish.sh
@@ -6,12 +6,12 @@ if [ "$WORDPRESS_ENVIRONMENT" = "remote-backup" ]; then
   echo "Start publishing service"
   echo "- - - - - - - - - - - - - - - - - - - - - - -"
 
-  curl -X POST -d '{}' "$PUBLISH_DEPLOY_HOOK"
-
-  curl --request POST \
-    --url "https://api.render.com/v1/services/$PUBLISH_SERVICE_ID/resume" \
-    --header 'Accept: application/json' \
-    --header "Authorization: Bearer $PUBLISH_API_TOKEN"
+  # curl -X POST -d '{}' "$PUBLISH_DEPLOY_HOOK"
+  # 
+  # curl --request POST \
+  #   --url "https://api.render.com/v1/services/$PUBLISH_SERVICE_ID/resume" \
+  #   --header 'Accept: application/json' \
+  #   --header "Authorization: Bearer $PUBLISH_API_TOKEN"
 
   echo "- - - - - - - - - - - - - - - - - - - - - - -"
   echo "Finished starting publishing service"
