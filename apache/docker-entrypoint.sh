b68f678dcf91f34f65211fe045b881051ce7682c
Jim Thoburn <jim@tobbi.co>
2022-08-04 21:10:53 -0700

    Link default HTML folder to WordPress

--- apache/docker-entrypoint.sh
+++ apache/docker-entrypoint.sh
@@ -3,6 +3,10 @@
 # Avoid “Another git process seems to be running in this repository” error
 rm -f /var/www/git-wordpress/.git/index.lock
 
+# Link default HTML folder to WordPress
+rm -df /var/www/html
+ln -s /var/www/git-wordpress/html /var/www/html
+
 # Run a script, without waiting for it to finish
 # https://unix.stackexchange.com/questions/86247/what-does-ampersand-mean-at-the-end-of-a-shell-script-line#answer-86253
 bash /usr/local/bin/docker-entrypoint-environment.sh &
