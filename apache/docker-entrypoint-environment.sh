cc79f032df3a4a6eb1a3e0320bae309295395239
Jim Thoburn <jim@tobbi.co>
2022-08-14 19:51:46 -0700

    Revert "Only update status if reset succeeds"
    This reverts commit 03803adbad962c586bd741525242b84c5909b8c5.

--- apache/docker-entrypoint-environment.sh
+++ apache/docker-entrypoint-environment.sh
@@ -60,10 +60,12 @@ fi
 # fi
 
 if [ "$WORDPRESS_ENVIRONMENT" = "replica" ]; then
-  bash /usr/local/bin/reset.sh && \
-  echo "- - - - - - - - - - - - - - - - - - - - - - -" && \
-  echo "Update backup status" && \
-  echo "- - - - - - - - - - - - - - - - - - - - - - -" && \
+  bash /usr/local/bin/reset.sh
+
+  echo "- - - - - - - - - - - - - - - - - - - - - - -"
+  echo "Update backup status"
+  echo "- - - - - - - - - - - - - - - - - - - - - - -"
+
   curl -X POST -d '{}' "$BACKUP_STATUS_DEPLOY_HOOK"
 
   # curl --request POST \
