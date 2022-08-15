c10796f91c04334b48aaa329a942154f7f2885a4
Jim Thoburn <jim@tobbi.co>
2022-08-14 19:55:53 -0700

    Revert "Follow example from official docker docs"
    This reverts commit 304fec76d0086b406e94308cc35cf5254258a306.

--- apache/docker-entrypoint.sh
+++ apache/docker-entrypoint.sh
@@ -9,19 +9,9 @@ if [ -d "/var/www/git-wordpress/html" ]; then
   ln -s /var/www/git-wordpress/html /var/www/html
 fi
 
-# https://docs.docker.com/config/containers/multi-service_container/
-
-# turn on bash's job control
-set -m
-
-# Start the primary process and put it in the background
-# https://stackoverflow.com/questions/32255814/what-purpose-does-using-exec-in-docker-entrypoint-scripts-serve/32261019#32261019
-exec "$@" &
-  
-# Start the post-deploy process
+# Run a script, without waiting for it to finish
 # https://unix.stackexchange.com/questions/86247/what-does-ampersand-mean-at-the-end-of-a-shell-script-line#answer-86253
-bash /usr/local/bin/docker-entrypoint-environment.sh
+bash /usr/local/bin/docker-entrypoint-environment.sh &
 
-# now we bring the primary process back into the foreground
-# and leave it there
-fg %1
+# Hand off to the CMD
+exec "$@"
